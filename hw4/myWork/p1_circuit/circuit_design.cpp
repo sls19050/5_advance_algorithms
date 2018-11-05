69d249bcb68d160b26c06b82fca645ff89872c85
sls19050 <sls19050@gmail.com>
2018-11-04 16:24:42 -0800

    finished p1 circuit
    addressed all test cases. note that this does not compile on windows using MinGW.

--- hw4/myWork/p1_circuit/circuit_design.cpp
+++ hw4/myWork/p1_circuit/circuit_design.cpp
@@ -1,15 +1,15 @@
+#include <sys/resource.h>
 #include <bits/stdc++.h>
 #include <iostream>
 #include <list>
 #include <stack>
 #include <cmath>
-#include <limits>
 #include <queue>
 #include <tuple>
+#include <set>
 
 using namespace std;
 
-//const int MAX_INT = std::numeric_limits<int>::max();
 const int NIL = -1;
 
 void die(const std::string& msg) {
@@ -30,19 +30,15 @@ public:
     int numVertices;
     int numEdges;
     vector<list<int>> adj;
-    queue<int> reverseTopOrderNodeID;
 
-    void getSCC(int u, vector<int>& disc, vector<int>& low,
-                stack<int>& st, vector<bool> stackMem, bool& pass);
+    void getSCC(int u, int disc[], int low[],
+                   stack<int> *st, bool stackMem[], bool& pass);
 
     Graph(int n, int m);
-    int convertIndex(int i);
     void clauseToEdge(int v, int w);
-    void SCC();
-    bool failSCC(int w, vector<int>& countVars);
-    bool isSatisfiable(vector<int>& result);
+    bool isSatisfiable();
     void showGraph();
-    tuple<int,int> getVarIDandSign(int curLit);
+    bool assignAns(int curLit, set<int>& countPosVars, set<int>& countNegVars);
 };
 
 Graph::Graph(int n, int m){
@@ -50,46 +46,58 @@ Graph::Graph(int n, int m){
     this->numVertices = 2*n;
     this->numEdges = 2*m;
     adj.resize(numVertices);
+    result.assign(numVar,0);
 }
 
-int Graph::convertIndex(int i){
-    if (i<0){
-        return -1*i+numVar-1;
-    } else{
-        return i-1;
-    }
-}
+bool Graph::assignAns(int curLit, set<int>& countPosVars, set<int>& countNegVars){
 
-bool Graph::failSCC(int w, vector<int>& countVars){
+    int var, sign, oldCount;
 
-    int varID, value, oldCount;
-    if (w>=numVar){
-        varID = w-numVar; //for negated literals
-        value = -1;
+    if (curLit>=numVar){
+        var = curLit-numVar; //for negated literals
+        sign = -1;
     } else{
-        varID = w;
-        value = 1;
+        var = curLit;       //for literals w/o negation
+        sign = 1;
     }
-    oldCount = abs(countVars[varID]);
-    countVars[varID] = countVars[varID] + value;
-    if (oldCount > abs(countVars[varID])){
-        return true;
+
+    if (sign > 0){
+        countPosVars.insert(var);
+    } else{
+        countNegVars.insert(var);
     }
-    return false;
+
+    bool is_inPos = countPosVars.find(var) != countPosVars.end();
+    bool is_inNeg = countNegVars.find(var) != countNegVars.end();
+    if (is_inPos && is_inNeg){ return false; }
+
+    if (result[var] == 0){
+        result[var] = sign;
+    }
+    return true;
 }
 
 void Graph::clauseToEdge(int l1, int l2){
-    int u, v;
-    //cout<<"clause to edge:"<<endl;
-    u = convertIndex(-l1);
-    v = convertIndex(l2);
-    //cout<<"    check edge: u, v = "<<u<<", "<<v<<endl;
-    //die("line 57");
-    adj[u].push_back(v);
-    u = convertIndex(-l2);
-    v = convertIndex(l1);
-    //cout<<"    check edge: u, v = "<<u<<", "<<v<<endl;
-    adj[u].push_back(v);
+    int u1, v2, u2, v1;
+
+    if (l1<0){
+        u1 = -l1-1;
+        v2 = -l1+numVar-1;
+    } else{
+        u1 = l1+numVar-1;
+        v2 = l1-1;
+    }
+
+    if (l2<0){
+        u2 = -l2-1;
+        v1 = -l2+numVar-1;
+    } else{
+        u2 = l2+numVar-1;
+        v1 = l2-1;
+    }
+
+    adj[u1].push_back(v1);
+    adj[u2].push_back(v2);
 }
 
 void Graph::showGraph(){
@@ -102,19 +110,17 @@ void Graph::showGraph(){
     }
 }
 
-void Graph::getSCC(int u, vector<int>& disc, vector<int>& low,
-                   stack<int>& st, vector<bool> stackMem, bool& pass){
+void Graph::getSCC(int u, int disc[], int low[],
+                   stack<int> *st, bool stackMem[], bool& pass){
 
     if(pass){
 
     static int time = 0;
 
-    //cout<<"recursive, at u = "<<u<<endl;
-
     time++;
     disc[u] = time;
     low[u] = time;
-    st.push(u);
+    st->push(u);
     stackMem[u] = true;
 
     list<int>::iterator i;
@@ -131,146 +137,93 @@ void Graph::getSCC(int u, vector<int>& disc, vector<int>& low,
     }
     int w = 0;
     if (low[u] == disc[u]){
-        vector<int> countVars(numVar, 0);
-        while (st.top() != u){
-            w = st.top();
-            //cout<<w<<" ";
-            reverseTopOrderNodeID.push(w);
-            if(failSCC(w, countVars)){
+        set<int> countPosVars;
+        set<int> countNegVars;
+
+        while (st->top() != u){
+            w = st->top();
+            if(!assignAns(w, countPosVars, countNegVars)){
                 pass = false;
-                //cout<<"\nfailed SCC! breaking now!\n";
                 break;
             }
             stackMem[w] = false;
-            st.pop();
+            st->pop();
         }
-        w = st.top();
-        //cout<< w<<endl;
-        reverseTopOrderNodeID.push(w);
-        if(failSCC(w, countVars)){
+
+        if(pass){
+
+        w = st->top();
+        if(!assignAns(w, countPosVars, countNegVars)){
             pass = false;
-            //cout<<"\nfailed SCC! breaking now!\n";
         }
         stackMem[w] = false;
-        st.pop();
+        st->pop();
+
+        }
     }
     }
 }
 
-tuple<int,int> Graph::getVarIDandSign(int curLit){
-    //cout<<"curLit = "<<curLit<<endl;
-    if (curLit>=numVar){
-        return make_tuple(curLit - numVar, -1);
-    }
-    return make_tuple(curLit, 1);
-}
+bool Graph::isSatisfiable(){
 
-bool Graph::isSatisfiable(vector<int>& result){
-    vector<int> disc(numVertices, NIL);
-    vector<int> low(numVertices, NIL);
-    vector<bool> stackMem(numVertices, false);
+    int *disc = new int[numVertices];
+    int *low = new int[numVertices];
+    bool *stackMem = new bool[numVertices];
+    stack<int> *st = new stack<int>();
+
     bool pass = true;
-    stack<int> st;
+
+    for (int i = 0; i < numVertices; i++) {
+        disc[i] = NIL;
+        low[i] = NIL;
+        stackMem[i] = false;
+    }
 
     for(int i = 0; i< numVertices; i++){
-        //cout<<"loop in SCC, i = "<<i<<endl;
         if(disc[i] == NIL){
             getSCC(i, disc, low, st, stackMem, pass);
         }
         if(!pass){return pass;}
     }
-
-    result.resize(numVar);
-    while (!reverseTopOrderNodeID.empty()){
-        int curLit, var, sign;
-        curLit = reverseTopOrderNodeID.front();
-        reverseTopOrderNodeID.pop();
-
-        tie(var, sign) = getVarIDandSign(curLit);
-        if (result[var] == 0){
-            result[var] = sign;
-        }
-    }
-
     return pass;
 }
 
-struct TwoSatisfiability {
-    int numVars;
-    vector<Clause> clauses;
+int main() {
 
-    TwoSatisfiability(int n, int m) :
-        numVars(n),
-        clauses(m)
-    {  }
-
-    bool isSatisfiable(vector<int>& result) {
-        // This solution tries all possible 2^n variable assignments.
-        // It is too slow to pass the problem.
-        // Implement a more efficient algorithm here.
-        for (int mask = 0; mask < (1 << numVars); ++mask) {
-            for (int i = 0; i < numVars; ++i) {
-                result[i] = (mask >> i) & 1;
-            }
-
-            bool formulaIsSatisfied = true;
-
-            for (const Clause& clause: clauses) {
-                bool clauseIsSatisfied = false;
-                if (result[abs(clause.firstVar) - 1] == (clause.firstVar < 0)) {
-                    clauseIsSatisfied = true;
-                }
-                if (result[abs(clause.secondVar) - 1] == (clause.secondVar < 0)) {
-                    clauseIsSatisfied = true;
-                }
-                if (!clauseIsSatisfied) {
-                    formulaIsSatisfied = false;
-                    break;
-                }
-            }
-
-            if (formulaIsSatisfied) {
-                return true;
+    const rlim_t kStackSize = 64L * 1024L * 1024L;  // min stack size = 64 Mb
+    struct rlimit rl;
+    int result;
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result == 0)
+    {
+        if (rl.rlim_cur < kStackSize)
+        {
+            rl.rlim_cur = kStackSize;
+            result = setrlimit(RLIMIT_STACK, &rl);
+            if (result != 0)
+            {
+                fprintf(stderr, "setrlimit returned result = %d\n", result);
             }
         }
-        return false;
     }
-};
 
-int main() {
     ios::sync_with_stdio(false);
 
     int n, m;
     cin >> n >> m;
-    //TwoSatisfiability twoSat(n, m);
+
     Graph twoSat(n, m);
     int firstVar, secondVar;
+
     for (int i = 0; i < m; ++i) {
-        //cin >> twoSat.clauses[i].firstVar >> twoSat.clauses[i].secondVar;
         cin >> firstVar >> secondVar;
         twoSat.clauseToEdge(firstVar, secondVar);
     }
 
-    //twoSat.showGraph();
-    //twoSat.SCC();
-
-    vector<int> result(n);
-    if (twoSat.isSatisfiable(result)) {
+    if (twoSat.isSatisfiable()) {
         cout << "SATISFIABLE" << endl;
         for (int i = 0; i < n; ++i) {
-            if(result[i]==0){
-                cout<<i+1;
-            } else{
-                cout<<result[i]*(i+1);
-                //cout<<result[i];
-            }
-
-
-            if (i < n-1) {
-                cout << " ";
-            } else {
-                cout << endl;
-            }
+            cout<<twoSat.result[i]*(i+1)<<" ";
         }
     } else {
         cout << "UNSATISFIABLE" << endl;
